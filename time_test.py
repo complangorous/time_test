ceb4fa170b6d64f72b59c290e48ffccfc13797a6
Sam <cwsamryan@gmail.com>
2018-03-28 10:39:27 -0700

    Added optional 'dest' argument, and --help text

--- time_test.py
+++ time_test.py
@@ -27,12 +27,21 @@ import os
 import time
 import pandas as pd
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(add_help=True)
 # 'scripts' is the full path to the .csv file containing the 
 # python scripts
-parser.add_argument('scripts', type=str)
-# 'num_tests' is the number of runtimes to record
-parser.add_argument('num_tests', type=int)
+parser.add_argument('scripts', type=str,
+		    help='The path of the .txt file containing the list of scripts to test.')
+# 'num_tests' is the number of run-times to record
+parser.add_argument('num_tests', type=int,
+		    help='The number of run-times to record.')
+# 'dest_path' is an optional argument
+# specifying the destination of the 
+# output file
+parser.add_argument('--dest', nargs='?',
+		    const=None, default=None,
+		    help='(Optional) Specify the destination of the output file. Default behavior
+			  is to write the file to the directory of time_test.py')
 args = parser.parse_args()
 
 # open file containing script list
@@ -52,9 +61,9 @@ for n in range(args.num_tests):
         runtime_cols.append('Runtime {}'.format(str(n+1)))
         columns_dict['Run {}: Successful Exit'.format(str(n+1))] = []
 
-for script in script:
+for script in scripts:
         for n in range(args.num_tests):
-                print 'Running ... {0} {1} {2}/{3}'.format(script, '\n', str(n+1), args.num_tests)
+                print '{0} Running ... {1} {2}/{3}'.format('\n', script,  str(n+1), args.num_tests)
                 start = time.time()
 
                 result = os.system('python {}'.format(script))
@@ -64,12 +73,12 @@ for script in script:
                         columns_dict['Run {}: Successful Exit'.format(str(n+1))].append(False)
                         runtime = time.time() - start
                         columns_dict['Runtime {}'.format(str(n+1))].append(runtime)
-                        print '{0} Error encountered while running {1} --- executed in {2} seconds {0}'.format('\n', script, runtime)
+                        print '{0} Error encountered while running {1} --- executed in {2:.2f} seconds {0}'.format('\n', script, runtime)
                 else:
 			columns_dict['Run {}: Successful Exit'.format(str(n+1))].append(True)
                         runtime = time.time() - start
                         columns_dict['Runtime {}'.format(str(n+1))].append(runtime)
-                        print '{0} Done executing {1} --- executed in {2} seconds {0}'.format('\n', script, runtime)
+                        print '{0} Done executing {1} --- executed in {2:.2f} seconds {0}'.format('\n', script, runtime)
 
         # get the average performance for the script, in minutes
         columns_dict['Average Case (Minutes)'].append((sum([columns_dict[x][-1] for x in runtime_cols]) / args.num_tests) / 60.0)
@@ -91,7 +100,7 @@ for n in range(args.num_tests):
         cols.append(runtimes.pop(0))
         cols.append(exit_statuses.pop(0))
 cols.append('Average Case (Minutes)')
-cols.append('Under timelimit')
+cols.append('Under time limit')
 
 df = df[cols]
 print df
@@ -100,7 +109,8 @@ from datetime import datetime
 curr_date = datetime.now().date()
 curr_date = str(curr_date)
 
-df.to_csv('{}_time_test_results.tsv'.format(curr_date), sep='\t', index=False)
-
-print '{} Done.'.format('\n')
+if args.dest is None:
+	df.to_csv('{1}_time_test_results.tsv'.format(curr_date), sep='\t', index=False)
+else:
+	df.to_csv('{0}/{1}_time_test_results.tsv'.format(args.dest, curr_date), sep='\t', index=False)
 
