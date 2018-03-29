eccbd2aff9854c1d55e1021d91479a195da61e27
Sam <cwsamryan@gmail.com>
2018-03-29 08:55:54 -0700

    Changed some spacing & comment structure to be more PEP-compliant

--- time_test.py
+++ time_test.py
@@ -7,18 +7,17 @@
         and writes the results to a tsv.
 
 	NOTE: If running time_test in an ssh sess-
-	ion, be sure to run it with nohup, 
-	
+	ion, be sure to run it with nohup,
+
 	$ nohup python time_test.py file_list.txt num_test &
 
 	if you are passing it an especially large
 	number of files to test, or are testing
-	them for an especially large value of 
+	them for an especially large value of
 	num-tests. Using nohup will detatch the
 	process from your console instance, so it
-	will continue running if the pipe breaks.	
+	will continue running if the pipe breaks.
 '''
-
 
 __author__ = 'sam.ryan'
 
@@ -28,33 +27,33 @@ import time
 import pandas as pd
 
 parser = argparse.ArgumentParser(add_help=True)
-# 'scripts' is the full path to the .csv file containing the 
-# python scripts
+# 'scripts' is the full path to the .csv file containing the
+# python scripts.
 parser.add_argument('scripts', type=str,
 		    help='The path of the .txt file containing the list of scripts to test.')
-# 'num_tests' is the number of run-times to record
+# 'num_tests' is the number of run-times to record.
 parser.add_argument('num_tests', type=int,
 		    help='The number of run-times to record.')
 # 'dest_path' is an optional argument
-# specifying the destination of the 
-# output file
+# specifying the destination of the
+# output file.
 parser.add_argument('--dest', nargs='?',
 		    const=None, default=None,
 		    help='(Optional) Specify the destination of the output file. Default behavior is to write the file to the directory of time_test.py')
 args = parser.parse_args()
 
-# open file containing script list
-# and remove newlines from entries
+# Open file containing script list
+# and remove newlines from entries.
 scripts = open(args.scripts).readlines()
 scripts = [script.rstrip('\n') for script in scripts]
 runtime_cols = []
 columns_dict = {'Script': [], 'Average Case (Minutes)': [], 'Ready to migrate': []}
 
-# populate columns_dict with num_tests pairs of
+# Populate columns_dict with num_tests pairs of,
 # <<Runtime {} / Run {}: Successful Exit>> columns
-# 
-# stash <<Runtime {}>> columns in runtime_cols for
-# calculating the average cases of each script
+#
+# Stash <<Runtime {}>> columns in runtime_cols for
+# calculating the average cases of each script.
 for n in range(args.num_tests):
         columns_dict['Runtime {}'.format(str(n+1))] = []
         runtime_cols.append('Runtime {}'.format(str(n+1)))
@@ -62,11 +61,14 @@ for n in range(args.num_tests):
 
 for script in scripts:
         for n in range(args.num_tests):
-                print('{0} Running ... {1} {2}/{3}'.format('\n', script,  str(n+1), args.num_tests))
+                print('{0} Running ... {1} {2}/{3}'.format('\n',
+                                                           script,
+                                                           str(n+1),
+                                                           args.num_tests))
                 start = time.time()
 
                 result = os.system('python {}'.format(script))
-                # if os.system('python {script}') returns 0, 
+                # If os.system('python {script}') returns 0,
                 # no error was encountered.
                 if result != 0:
                         columns_dict['Run {}: Successful Exit'.format(str(n+1))].append(False)
@@ -79,21 +81,25 @@ for script in scripts:
                         columns_dict['Runtime {}'.format(str(n+1))].append(runtime)
                         print('{0} Done executing {1} --- executed in {2:.2f} seconds {0}'.format('\n', script, runtime))
 
-        # get the average performance for the script, in minutes
-        columns_dict['Average Case (Minutes)'].append((sum([columns_dict[x][-1] for x in runtime_cols]) / args.num_tests) / 60.0)
+        # Get the average performance for the script, in minutes.
+        columns_dict['Average Case (Minutes)'].append((sum([columns_dict[x][-1] for x in runtime_cols])
+                                                       / args.num_tests) / 60.0)
         columns_dict['Script'].append(script)
 
-        exit_statuses = [columns_dict[p][-1] for p in [q for q in columns_dict.keys() if 'Success' in q]]
+        exit_statuses = [columns_dict[p][-1] for p in
+                                            [q for q in columns_dict.keys()
+                                             if 'Success' in q]]
         if False in exit_statuses:
                 columns_dict['Ready to migrate'].append(False)
         else:
                 columns_dict['Ready to migrate'].append(True)
 
-columns_dict['Under time limit'] = [(True if z < 5.0 else False) for z in columns_dict['Average Case (Minutes)']]
+columns_dict['Under time limit'] = [(True if z < 5.0 else False)
+                                     for z in columns_dict['Average Case (Minutes)']]
 df = pd.DataFrame(columns_dict)
 
-# orders the columns corresponding to
-# each run so they follow the pattern -
+# Orders the columns corresponding to
+# each run so they follow the pattern:
 # Runtime {} | Runtime {}: Successful Exit
 exit_statuses = [x for x in df.columns.tolist() if 'Successful' in x]
 exit_statuses.sort()
@@ -118,5 +124,6 @@ curr_date = str(curr_date)
 if args.dest is None:
 	df.to_csv('{0}_time_test_results.tsv'.format(curr_date), sep='\t', index=False)
 else:
-	df.to_csv('{0}/{1}_time_test_results.tsv'.format(args.dest, curr_date), sep='\t', index=False)
-
+	df.to_csv('{0}/{1}_time_test_results.tsv'.format(args.dest, curr_date),
+                                              sep='\t',
+                                              index=False)
